56009a226eae12a34f7c34508b6c982f9453a1b0
Ash50684 <ashishk50684@gmail.com>
2026-03-16 16:18:59 +0530

    UC1 - Application Entry & Welcome Message

--- src/UseCase1HotelBookingApp.java
+++ src/UseCase1HotelBookingApp.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class UseCase1HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("===================================");
+        System.out.println("Welcome to Book My Stay App");
+        System.out.println("Hotel Booking System v1.0");
+        System.out.println("===================================");
+
+    }
 }
